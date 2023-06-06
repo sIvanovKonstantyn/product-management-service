6ec9cb69e6efe801c0fd4c7b04e344992ee38227
Kostiantyn_Ivanov <ICKostiantyn.Ivanov@amwell.com>
2023-06-06 15:13:45 +0200

    initial

---
+++ src/test/java/com/demo/pms/ProductManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
