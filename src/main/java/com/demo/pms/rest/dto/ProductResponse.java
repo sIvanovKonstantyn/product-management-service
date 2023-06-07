1fbe8f1b9a4d4655fb8a50cf3dedec559e27764f
Kostiantyn_Ivanov <ICKostiantyn.Ivanov@amwell.com>
2023-06-07 13:53:00 +0200

    Fix product response dto

--- src/main/java/com/demo/pms/rest/dto/ProductResponse.java
+++ src/main/java/com/demo/pms/rest/dto/ProductResponse.java
@@ -1,10 +1,14 @@
 package com.demo.pms.rest.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponse {
     private Long id;
     private String code;
